fadc60ccd6cfb94f2352ffff01ffcbda66c7afe3
itzibtihal <ibtihalboukhanchouch@gmail.com>
2024-01-31 10:10:04 +0100

    fix create book front

--- laravel-LIB/resources/views/books/create.blade.php
+++ laravel-LIB/resources/views/books/create.blade.php
@@ -4,69 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartLibra</title>
+    <style>
+        body{color: #000;overflow-x: hidden;height: 100vh;background-image: url("https://wallpaperaccess.com/full/253342.jpg");background-repeat: no-repeat;background-size: 100% 100%}.card{padding: 30px 40px;margin-top: 60px;margin-bottom: 60px;border: none !important;box-shadow: 0 6px 12px 0 rgba(0,0,0,0.2)}.blue-text{color: #00BCD4}.form-control-label{margin-bottom: 0}input, textarea, button{padding: 8px 15px;border-radius: 5px !important;margin: 5px 0px;box-sizing: border-box;border: 1px solid #ccc;font-size: 18px !important;font-weight: 300}input:focus, textarea:focus{-moz-box-shadow: none !important;-webkit-box-shadow: none !important;box-shadow: none !important;border: 1px solid #00BCD4;outline-width: 0;font-weight: 400}.btn-block{text-transform: uppercase;font-size: 15px !important;font-weight: 400;height: 43px;cursor: pointer}.btn-block:hover{color: #fff !important}button:focus{-moz-box-shadow: none !important;-webkit-box-shadow: none !important;box-shadow: none !important;outline-width: 0}
+    </style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
 </head>
 <body>
-    <h1>Create Book to SmartLibra</h1>
-    <div>
-        @if($errors->any())
-          <ul>
-            @foreach($errors->all() as $error)
-               <li>{{$error}}</li>
-            @endforeach
-          </ul>
-        @endif
+    <div class="container-fluid px-1 py-5 mx-auto">
+        <div class="row d-flex justify-content-center">
+            <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
+                <h3 class="text-white">Add new Book to SmartLibra</h3>
+                <p class="text-white">Empower your library curation effortlessly <br> Elevate your library management experience with simplicity and efficiency.</p>
+                <div class="card">
+                    <h5 class="text-center mb-4">Add Book!</h5>
+                    <div>
+                        @if($errors->any())
+                          <ul>
+                            @foreach($errors->all() as $error)
+                               <li>{{$error}}</li>
+                            @endforeach
+                          </ul>
+                        @endif
+                    </div>
+                    <form class="form-card" action="{{route('book.store')}}" method="post"  enctype="multipart/form-data" >
+                        @csrf
+                        @method('post')
+                        <div class="row justify-content-between text-left">
+                            <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Title<span class="text-danger"> *</span></label> <input type="text"  name="title"  > </div>
+                            <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Author<span class="text-danger"> *</span></label> <input type="text"  name="author"  > </div>
+                        </div>
+                        <div class="row justify-content-between text-left">
+                            <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Genre<span class="text-danger"> *</span></label> <input type="text"  name="genre" placeholder=""> </div>
+                            <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Publication year<span class="text-danger"> *</span></label> <input type="date"  name="publication_year" placeholder=""> </div>
+                        </div>
+                        <div class="row justify-content-between text-left">
+                            <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Total copies<span class="text-danger"> *</span></label> <input type="text" name="total_copies" placeholder="" > </div>
+                            <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Available Copies<span class="text-danger"> *</span></label> <input type="text" name="available_copies" placeholder="" > </div>
+                        </div>
+
+                        <div class="row justify-content-between text-left">
+                            <div class="form-group col-12 flex-column d-flex"> <label class="form-control-label px-6">Description<span class="text-danger"> *</span></label> <textarea name="description" id="" cols="10" rows="5"></textarea> </div>
+                        </div>
+
+                        <div class="row justify-content-between text-left">
+                            <div class="form-group col-12 flex-column d-flex"> <label class="form-control-label px-3">Add Cover<span class="text-danger"> *</span></label> <input type="file"  name="image" placeholder="" > </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="form-group col-sm-6"> <button type="submit" class="btn-block " style="background-color: bisque;">Save</button> </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    <form action="{{route('book.store')}}" method="post"  enctype="multipart/form-data">
-        @csrf
-        @method('post')
-        <div>
-            <label for="">title</label>
-            <input type="text" name='title' placeholder="title">
-        </div>
-
-        <div>
-        <input type="file" name="image">
-        <button type="submit">Upload Image</button>
-        </div>
-
-        <div>
-            <label for="">Author</label>
-            <input type="text" name='author' placeholder="author">
-        </div>
-
-        <div>
-            <label for="">genre</label>
-            <input type="text" name='genre' placeholder="genre">
-        </div>
-
-        
-         
-        <div>
-            <label for="">publication year</label>
-            <input type="date" name='publication_year' >
-        </div>
-
-        <div>
-            <label for="">total copies</label>
-            <input type="number" name='total_copies' >
-        </div>
-
-        <div>
-            <label for="">availabe copies</label>
-            <input type="number" name='available_copies'>
-        </div>
-
-        
-        <div>
-            <label for="">description</label>
-            <!-- <textarea type="text" name='description' placeholder="description"> -->
-                <textarea name="description" id="" cols="30" rows="10"></textarea>
-        </div>
-       
-        <div>
-            <input type="submit" value="Save new book">
-        </div>
-    </form>
-    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
+
 </html>
